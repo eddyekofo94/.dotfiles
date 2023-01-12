3ea33a8ff31d944e2c423852ab45b0bcf82675ee
Eddy Ekofo <eddyecko@gmail.com>
2023-01-12 02:25:55 +0100

    Change fish & Tide config

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,5 @@
 ######################################
-# 
+#
 # Athor: Eddy Ekofo - WSL fish configs
 #
 ######################################
@@ -10,15 +10,20 @@ end
 
 if status is-login
 
-    # INFO: To get WSL 2 working with VPN https://github.com/sakai135/wsl-vpnkit
-    wsl.exe -d wsl-vpnkit service wsl-vpnkit start
-    # For Homebrew/Linuxbrew to work
-    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-    
+    if grep -qi microsoft /proc/version
+        echo "It is wsl"
+        # For Homebrew/Linuxbrew to work
+        eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+        # WSL specidic aliases & abbrs
+        alias docker='docker.exe'
+    end
+
+
     # Cargo: for Rust development
     # set -Ua fis_user_paths $HOME/.cargo/bin
     export PATH="$PATH:$HOME/.cargo/bin"
-    
+
     # Zoxide a smart cd to directories (needs rust)
     zoxide init fish | source
 
@@ -28,34 +33,34 @@ if status is-login
     # Explicitly define them here so we don't need to add the additional code of
     # handling the case where they are not explicitly defined, simplifying the code
     # in the rest of our configurations which use XDG.
-    
+
     # Directory where user-specific data files should be stored.
     export XDG_DATA_HOME="$HOME/.local/share"
-    
+
     # Preference-ordered set of base directories to search for data files in
     # addition to the $XDG_DATA_HOME base directory.
     export XDG_DATA_DIRS="/usr/local/share/:/usr/share/"
-    
+
     # Directory where user-specific configuration files should be stored.
     export XDG_CONFIG_HOME="$HOME/.config"
-    
+
     # Preference-ordered set of base directories to search for configuration files
     # in addition to the $XDG_CONFIG_HOME base directory.
     #export XDG_CONFIG_DIRS="/etc/xdg"
-    
+
     # Directory where user-specific non-essential data files should be stored.
     export XDG_CACHE_HOME="$HOME/.cache"
-    
+
     # export JAVA_HOME="/usr/libexec/java_home"
     export JAVA_HOME="$SDKMAN_DIR/candidates/java/current"
-    
+
     # LS colors using Vivid installed using Cargo
-    export LS_COLORS="$(vivid generate one-dark)"
-    
+    export LS_COLORS="$(vivid generate $HOME/.dotfiles/vivid/everforest.yml)"
+
     # FZF
     export FZF_DEFAULT_COMMAND='rg --files --no-ignore-vcs --no-require-git --no-ignore --hidden --follow --glob "!.git/*"'
     export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-    
+
     # Bat a modern cat with all the goodies
     export BAT_CONFIG_PATH=$HOME/.dotfiles/bat/lib/login/bat.conf
 end
@@ -91,7 +96,7 @@ abbr -a -U -- vim nvim
 abbr -a -U -- bgr batgrep
 abbr -a -U -- bman batman
 
-# Cargo aliases 
+# Cargo aliases
 abbr -a -U -- cg cargo
 abbr -a -U -- cgc 'cargo clean'
 abbr -a -U -- cgi 'cargo install'
@@ -106,5 +111,3 @@ abbr -a -U -- cgug 'cargo upgrade'
 bind ! __history_previous_command
 bind '$' __history_previous_command_arguments
 
-# WSL specidic aliases & abbrs
-alias docker='docker.exe'
--- fish/functions/_tide_pwd.fish
+++ fish/functions/_tide_pwd.fish
@@ -7,7 +7,7 @@ set -l home_icon $tide_pwd_icon_home' '
 set -l pwd_icon $tide_pwd_icon' '
 
 eval "function _tide_pwd
-    if set -l split_pwd (string replace -r '^$HOME' '~' \$PWD | string split /)
+    if set -l split_pwd (string replace -r '^$HOME' '~' -- \$PWD | string split /)
         test -w . && set -f split_output \"$pwd_icon\$split_pwd[1]\" \$split_pwd[2..] ||
             set -f split_output \"$unwritable_icon\$split_pwd[1]\" \$split_pwd[2..]
         set split_output[-1] \"$color_anchors\$split_output[-1]$reset_to_color_dirs\"
@@ -15,20 +15,18 @@ eval "function _tide_pwd
         set -f split_output \"$home_icon$color_anchors~\"
     end
 
-    string join / \$split_output | string length -V | read -g _tide_pwd_len
+    string join / -- \$split_output | string length -V | read -g _tide_pwd_len
 
     i=1 for dir_section in \$split_pwd[2..-2]
         string join -- / \$split_pwd[..\$i] | string replace '~' $HOME | read -l parent_dir # Uses i before increment
 
         math \$i+1 | read i
 
-        if false || for marker in $tide_pwd_markers # false is for if tide_pwd_markers is empty
-                test -e \$parent_dir/\$dir_section/\$marker && break
-            end
+        if path is \$parent_dir/\$dir_section/\$tide_pwd_markers
             set split_output[\$i] \"$color_anchors\$dir_section$reset_to_color_dirs\"
         else if test \$_tide_pwd_len -gt \$dist_btwn_sides
             set -l trunc
-            while string match -qr \"(?<trunc>\$trunc.)\" \$dir_section && test (count \$parent_dir/\$trunc*/) != 1
+            while string match -qr \"(?<trunc>\$trunc.)\" \$dir_section && v=\$parent_dir/\$trunc*/ set -q v[2]
             end
             test -n \"\$trunc\" && set split_output[\$i] \"$color_truncated\$trunc$reset_to_color_dirs\" &&
                 string join / \$split_output | string length -V | read _tide_pwd_len
