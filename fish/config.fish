b3d12ec748773d58c4fa9784df5765e9d52f37fc
Eddy Ekofo <eddy.ekofo@amadeus.com>
2023-08-16 09:31:48 +0000

    Zellij starting on fish shell also

--- fish/config.fish
+++ fish/config.fish
@@ -23,6 +23,7 @@ end
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    eval (zellij setup --generate-auto-start fish | string collect)
 end
 
 if status is-login
