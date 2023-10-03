faefb3150281f2ffbc1bee468b118a9c31ee2597
Eddy Ekofo <eddy.ekofo@amadeus.com>
2023-10-03 15:42:51 +0000

    Improve some config

--- zsh/helpers/fzf.zsh
+++ zsh/helpers/fzf.zsh
@@ -86,7 +86,7 @@ if has fd; then
 
     # FZF
     # Setting fd as the default source for fzf
-    export FZF_DEFAULT_COMMAND='fd --type f --strip-cwd-prefix --hidden --follow --exclude .git/.npm'
+    export FZF_DEFAULT_COMMAND='fd --type f --strip-cwd-prefix --hidden --follow --exclude .git'
     export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
     export FZF_ALT_C_OPTS="--preview 'exa -T {}' --height=60%"
